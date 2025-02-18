fc3ff4bf08495c8599dc8db20ec5bdd32ae58f76
Aaron <aaronf09@outlook.com>
2025-02-18 17:45:23 -0600

    Update db.php

--- yii2Rental/config/db.php
+++ yii2Rental/config/db.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
+    /*
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=dbYii2_Rental',
     'username' => 'root',
     'password' => '1234',
     'charset' => 'utf8',
-
-    /*
+    */
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=dbYii2_Rental',
     'username' => 'root',
     'password' => 'P4ulitaDBP4ss!!!',
     'charset' => 'utf8',
-    */
+    
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
